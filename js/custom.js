d4e9e36902ab7d4941a5eeef7fe583b3e3134ce1
sadegh3333 <sadegh3333@ggmail.com>
2017-11-25 03:35:02 +0330

    first init

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+// Header controller
+$(document).ready(function () {
+$('custom',function($scope) {
+    // Make Persian Date
+    var now = persianDate();
+    $('.date').html(persianDate().format('dddd  DD MMMM YYYY | ss : mm : HH '));
+    setInterval(function () {
+        var now = persianDate();
+        $('.date').html(persianDate().format('dddd  DD MMMM YYYY | ss : mm : HH '));
+    }, 1000);});
+
+
+
+
+
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36251023-1']);
+    _gaq.push(['_setDomainName', 'jqueryscript.net']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+});
+
+
+
+$(function(){
+    $('.parascroll').parascroll();
+})
